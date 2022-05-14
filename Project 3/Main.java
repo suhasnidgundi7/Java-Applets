6f7b8e9596fb36dff6b8b8706c54a609f67e91a9
Suhas Nidgundi <suhasnidgundi@gmail.com>
2022-05-14 21:46:03 +0530

    Write output of the Program

---
+++ Project 3/Main.java
@@ -0,0 +1,10 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class Main extends Applet
+{
+    @Override
+    public void paint(Graphics g) { 
+        setBackground(Color.red);
+    }
+}
